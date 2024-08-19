5786e16b680bb40869f23fe4c61115fa7a9a7207
Ziad Salah <zslah2800@gmail.com>
2024-08-19 15:39:38 +0300

    home view body

--- lib/home/home_view.dart
+++ lib/home/home_view.dart
@@ -1,3 +1,4 @@
+import 'package:crud_app/home/widgets/home_view_body.dart';
 import 'package:flutter/material.dart';
 
 class HomeView extends StatelessWidget {
@@ -5,6 +6,11 @@ class HomeView extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold();
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Flutter CRUD'),
+      ),
+      body: const HomeViewBody(),
+    );
   }
 }
