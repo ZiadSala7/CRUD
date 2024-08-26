0ad648b6d8baf89916e4564cd2ebc9b8bd8e1c52
ZiadSala7 <zslah2800@gmail.com>
2024-08-26 16:07:40 +0300

    update and create

--- lib/add data/data/db helper/db_helper.dart
+++ lib/add data/data/db helper/db_helper.dart
@@ -1,14 +1,39 @@
+import 'package:crud_app/add%20data/data/models/crud_model.dart';
 import 'package:sqflite/sqflite.dart';
 import 'package:path/path.dart';
+import 'dart:async';
 
 class DbHelper {
-  get onCreateMethod => null;
-
+  Database? _db;
   initDB() async {
     String databasePath = await getDatabasesPath();
     String path = join(databasePath, "dbase.db");
 
     var db = await openDatabase(path, version: 1, onCreate: onCreateMethod);
     return db;
+  }
+
+  onCreateMethod(Database db, int version) async {
+    await db.execute(
+        "CREATE TABLE Home(name TEXT, description TEXT, price DOUBLE)");
+  }
+
+  Future<Database?> get db async {
+    _db ??= await initDB();
+    return _db;
+  }
+
+  Future<int> createMethod(CrudModel model) async {
+    var dbReady = await db;
+    int response = await dbReady!.rawInsert(
+        "INSERT INTO Home(name, description, price) VALUES (${model.name}, ${model.description}, ${model.price})");
+    return response;
+  }
+
+  Future<int> updateMethod(CrudModel model) async {
+    var dbReady = await db;
+    int response = await dbReady!.rawInsert(
+        "UPDATE Home WHERE name = ${model.name}, description = ${model.description}, price = ${model.description}");
+    return response;
   }
 }
