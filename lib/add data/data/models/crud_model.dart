9eec943c64af2689ebf1cb4e6a044c7dd8545ec1
ZiadSala7 <zslah2800@gmail.com>
2024-08-26 12:45:43 +0300

    moving and model created

---
+++ lib/add data/data/models/crud_model.dart
@@ -0,0 +1,10 @@
+class CrudModel {
+  final String name, description;
+  final double price;
+
+  CrudModel({
+    required this.name,
+    required this.description,
+    required this.price,
+  });
+}
